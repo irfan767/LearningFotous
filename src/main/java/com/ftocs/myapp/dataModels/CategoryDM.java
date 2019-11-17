e45dd5e650bfa38a2f4fd2e400b39930dfbbadd0
Ftous app <irfanyaqub71@gmail.com>
2019-11-18 00:04:11 +0500

    local changes

--- src/main/java/com/ftocs/myapp/dataModels/CategoryDM.java
+++ src/main/java/com/ftocs/myapp/dataModels/CategoryDM.java
@@ -10,7 +10,7 @@ public class CategoryDM{
 	@SerializedName("IsSuccess")
 	private boolean isSuccess;
 
-	@SerializedName("List")
+	@SerializedName("Listt")
 	private List<ListItem> list;
 
 	@SerializedName("$id")
